a7ec9e66f14decd4b4294b93d49c80028cd5d652
szcrete <40736535+szcrete@users.noreply.github.com>
2018-08-28 20:29:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hey its Python Flask application! Bonjour Justin!'
 
-if __name__ == '__main__':
-  app.run()
+urls = (
+    '/users', 'list_users',
+    '/users/(.*)', 'get_user'
+)
+
+
+
+app = web.application(urls, globals())
+
+class list_users:        
+    def GET(self):
+      output = 'users:[child]';
+      return output
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+#@app.route('/')
+#def hello_world():
+#  return 'Hey its Python Flask application! Bonjour Justin!'
+
+#if __name__ == '__main__':
+#  app.run()
