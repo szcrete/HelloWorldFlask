bd41df8274850e532d2cd6f3535e33a1031e1457
szcrete <40736535+szcrete@users.noreply.github.com>
2018-08-28 20:56:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ tasks = [
     {
         'id': 2,
         'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
+        'description': u'Need to find a good Python tutorial on the web.', 
         'done': False
     }
 ]
